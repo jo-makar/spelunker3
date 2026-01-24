78d9bd74df233d49d1cd2ae91fad1f2d8b8abeff
J. O. Makar <jomakar@gmail.com>
2026-01-24 18:04:14 -0500

    chrome support

--- src/main/kotlin/Chrome.kt
+++ src/main/kotlin/Chrome.kt
@@ -6,63 +6,112 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.nio.file.Files
 import java.nio.file.Path
+import java.util.concurrent.TimeoutException
 import java.util.concurrent.TimeUnit
+import java.util.Scanner
 
 import kotlin.io.path.deleteRecursively
+import kotlin.io.path.outputStream
 import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-class Chrome(profile: String = "scraper", headless: Boolean = true): AutoCloseable {
-    private val tempDir: Path
-
-    init {
-        var complete = false
-        var _tempDir: Path? = null
-
-        try {
-            _tempDir = Files.createTempDirectory("spelunker3-chrome-")
-            logger.info { "temp dir: $_tempDir" }
-
-            val pipePath = _tempDir.resolve("pipe").toString()
-            if (!executeCommand(listOf("mkfifo", pipePath), 5000)) {
-                throw IllegalStateException("mkfifo failed")
-            }
-
-            // FIXME STOPPED Use mkfifo to create named pipes in (init)TempDir
-            //               for later use with the google-chrome command
-            //               (eg sh -c 'google-chrome ... 3>/tmp/...abc 4</tmp/...def')
-
-            val command: List<String> = buildList {
-                add("google-chrome")
-                add("--remote-debugging-pipe")
-                // A non-standard --user-data-dir is required for use with --remote-debugging-port/pipe
-                // Ref: https://developer.chrome.com/blog/remote-debugging-port
-                add("--user-data-dir=${System.getenv("HOME")!!}/.config/google-chrome-$profile")
-                if (headless) {
-                    add("--headless")
-                }
-            }
-
-            // FIXME Use java.lang.ProcessBuilder to launch chrome
-            //ProcessBuilder(command)
-
-            complete = true
-            tempDir = _tempDir
-        } finally {
-            if (!complete) {
-                _tempDir?.deleteRecursively()
-            }
-        }
-    }
-
-    override fun close() {
-        tempDir.deleteRecursively()
-    }
-
+class Chrome private constructor(
+    private val scope: CoroutineScope,
+    private val tempDir: Path,
+    private val process: Process
+): AutoCloseable {
     companion object {
         private val logger = KotlinLogging.logger {}
 
-        private fun executeCommand(command: List<String>, timeoutMs: Long): Boolean {
+        suspend fun create(
+            scope: CoroutineScope = CoroutineScope(Dispatchers.Default),
+            profile: String = "scraper",
+            headless: Boolean = true
+        ): Chrome {
+            var success = false
+            var tempDir: Path? = null
+            var process: Process? = null
+
+            try {
+                tempDir = Files.createTempDirectory("spelunker3-chrome-")
+                logger.info { "temp dir: $tempDir" }
+
+                val inputPipePath = tempDir.resolve("input-pipe")
+                val outputPipePath = tempDir.resolve("output-pipe")
+                for (pipePath in listOf(inputPipePath.toString(), outputPipePath.toString())) {
+                    if (!executeCommand(listOf("mkfifo", pipePath))) {
+                        throw IllegalStateException("mkfifo failed")
+                    }
+                }
+
+                val command = buildString {
+                    append("google-chrome ")
+                    append("--remote-debugging-port ")
+                    // A non-standard --user-data-dir is required for use with --remote-debugging-port/pipe
+                    // Ref: https://developer.chrome.com/blog/remote-debugging-port
+                    append("--user-data-dir=${System.getenv("HOME")!!}/.config/google-chrome-$profile ")
+                    if (headless) {
+                        append("--headless ")
+                    }
+                    append("3<$inputPipePath 4>$outputPipePath")
+                }
+
+                process = ProcessBuilder("sh", "-c", command)
+                    .redirectErrorStream(true)
+                    .start()
+                logger.info { "process started with pid = ${process.pid()}" }
+
+                scope.launch {
+                    BufferedReader(InputStreamReader(process.inputStream)).use { reader ->
+                        while (true) {
+                            val line = reader.readLine() ?: break
+                            logger.info { "stdout/err: $line" }
+                        }
+                    }
+                }
+
+                scope.launch {
+                    // Ref: https://github.com/cyrus-and/chrome-remote-interface/issues/381
+                    Scanner(outputPipePath.toFile()).use { scanner ->
+                        scanner.useDelimiter("\u0000")
+                        while (scanner.hasNext()) {
+                            val line = scanner.next()
+                            logger.info { "input: $line" }
+                            // FIXME Write to a circular buffer (guarded by a mutex)
+                        }
+                        logger.warn { "input pipe eof" }
+                    }
+                }
+
+                scope.launch {
+                    // FIXME Read from a queue (guarded by a mutex?) and write to the output stream
+                    inputPipePath.outputStream().use { outputStream ->
+                        // FIXME outputStream.write(/*...*/.toByteArray()) then flush()
+                    }
+                }
+
+                delay(15000)
+
+                success = true
+            } finally {
+                if (!success) {
+                    scope.cancel()
+                    process?.let {
+                        it.descendants().forEach(ProcessHandle::destroyForcibly)
+                        it.destroyForcibly()
+                    }
+                    tempDir?.deleteRecursively()
+                }
+            }
+            return Chrome(scope, tempDir, process)
+        }
+
+        private fun executeCommand(command: List<String>, timeoutMs: Long = 5000): Boolean {
             return runBlocking {
                 val process = ProcessBuilder(command)
                     .redirectErrorStream(true)
@@ -94,5 +143,26 @@ class Chrome(profile: String = "scraper", headless: Boolean = true): AutoCloseab
                 return@runBlocking true
             }
         }
+    }
+
+    override fun close() {
+        scope.cancel()
+
+        process.let {
+            fun destroy(handle: ProcessHandle, timeoutMs: Long = 15000) {
+                handle.destroy()
+                try {
+                    handle.onExit().get(timeoutMs, TimeUnit.MILLISECONDS)
+                } catch (_: TimeoutException) {
+                    logger.error { "process ${handle.pid()} destroy timeout, forcibly destroyed" }
+                    handle.destroyForcibly()
+                }
+            }
+
+            it.descendants().forEach { handle -> destroy(handle) }
+            destroy(it.toHandle())
+        }
+
+        tempDir.deleteRecursively()
     }
 }
--- src/main/kotlin/MainBrowserDemo.kt
+++ src/main/kotlin/MainBrowserDemo.kt
@@ -2,10 +2,12 @@ package com.github.jo_makar
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 
+import kotlinx.coroutines.runBlocking
+
 private val logger = KotlinLogging.logger {}
 
-fun main() {
-    Chrome().use { chrome ->
+fun main() = runBlocking {
+    Chrome.create().use { chrome ->
         // FIXME
     }
 }
